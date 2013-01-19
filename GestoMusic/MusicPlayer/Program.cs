75c978ef1b8c09625214922344613bb5176e5cbf
Anton_Work <anton.kuntsevich@gmail.com>
2013-01-19 18:33:03 +0300

    Added feature to play single samples

---
+++ GestoMusic/MusicPlayer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+
+namespace MusicPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new MusicPlayer();
+
+            player.Clap();
+
+            Console.ReadLine();
+        }
+    }
+}
