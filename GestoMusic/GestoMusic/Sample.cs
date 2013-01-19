204c9e3e8a7f2047ceb9827dbf9cabf21a1dd953
Anton_Work <anton.kuntsevich@gmail.com>
2013-01-19 19:14:51 +0300

    Merged music with gestures

--- GestoMusic/GestoMusic/Sample.cs
+++ GestoMusic/GestoMusic/Sample.cs
@@ -1,10 +1,30 @@
-﻿namespace GestoMusic
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace GestoMusic
 {
-    public class Sample
+    public class Sample : IDisposable
     {
+        private readonly string _sample;
+        private readonly FileStream _audio;
+
+        public Sample(string sample)
+        {
+            _sample = sample;
+
+            _audio = new FileStream(_sample, FileMode.Open);
+        }
+
         public void Play()
         {
-            throw new System.NotImplementedException();
+            var player = new SoundPlayer(_audio);
+            player.Play();
+        }
+
+        public void Dispose()
+        {
+            _audio.Dispose();
         }
     }
 }
