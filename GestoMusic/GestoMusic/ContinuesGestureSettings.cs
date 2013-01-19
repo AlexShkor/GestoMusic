8a06f2a8ce4146bbaaa9f21d4d0231e3e0f64253
Alex Shkor <alex.shkor@paralect.com>
2013-01-20 01:05:07 +0300

    exclude list initialized

--- GestoMusic/GestoMusic/ContinuesGestureSettings.cs
+++ GestoMusic/GestoMusic/ContinuesGestureSettings.cs
@@ -6,69 +6,6 @@ using Microsoft.Kinect;
 
 namespace GestoMusic
 {
-    public class HandUpContiniousGestureSettings : ContinuesGestureSettings
-    {
-
-        public const float PitchDetla = 0.6f;
-        private bool _isRightHandUp = false;
-        private bool _isLeftHandUp = false;
-
-        public HandUpContiniousGestureSettings()
-        {
-            SkeletonAdjustment += (sender, args) =>
-                {
-
-                    Pitch = (0.4f - (args.Skeleton.Joints[JointType.WristLeft].Position.X -
-                             args.Skeleton.Joints[JointType.WristRight].Position.X) * PitchDetla);
-                };
-        }
-
-        protected override ContinuesGestureResult GetActiveStatus(GestureType gestureType)
-        {
-            switch (gestureType)
-            {
-                case GestureType.DownHandLeft:
-                    _isLeftHandUp = false;
-                    break;
-                case GestureType.DownHandRigth:
-                    _isRightHandUp = false;
-                    break;
-                case GestureType.UpHandRight:
-                    _isRightHandUp = true;
-                    break;
-                case GestureType.UpHandLeft:
-                    _isLeftHandUp = true;
-                    break;
-                default:
-                    return ContinuesGestureResult.None;
-            }
-            if (_isLeftHandUp && _isRightHandUp)
-            {
-                if (IsTracked)
-                {
-                    return ContinuesGestureResult.None;
-                }
-                else
-                {
-                    return ContinuesGestureResult.Activate;
-                }
-            }
-            else
-            {
-                if (IsTracked)
-                {
-                    return ContinuesGestureResult.Deactivate;
-                }
-                else
-                {
-                    return ContinuesGestureResult.None;
-                }
-            }
-            return ContinuesGestureResult.None;
-        }
-
-    }
-
     public class ContinuesGestureSettings
     {
 
