6dda8e3856537bb9104922b7d635a287b2d8ac7c
Alex Shkor <alex.shkor@paralect.com>
2013-01-19 18:48:46 +0300

    sample project added

--- GestoMusic/GestoMusic/GesturesObserver.cs
+++ GestoMusic/GestoMusic/GesturesObserver.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using Fizbin.Kinect.Gestures;
 
 namespace GestoMusic
 {
     public class GesturesObserver
     {
-        public void TrackGesture(GestureTypeEnum rightHand, Action action)
+        private readonly Dictionary<GestureType, Sample> _getsturesActions = new Dictionary<GestureType, Sample>();
+        private readonly GestureController _gestureController;
+
+        public GesturesObserver()
         {
-            throw new NotImplementedException();
+            _gestureController = new GestureController();
+            _gestureController.GestureRecognized += OnGestureRecognized;
+        }
+
+        private void OnGestureRecognized(object sender, GestureEventArgs e)
+        {
+            if (_getsturesActions.ContainsKey(e.GestureType))
+            {
+                var sample = _getsturesActions[e.GestureType];
+                sample.Play();
+                Console.WriteLine("{0} recognized. {1} is playing.", e.GestureType, sample);
+            }
+        }
+
+        public void TrackGesture(GestureType rightHand, Sample sample)
+        {
+            _getsturesActions[rightHand] = sample;
         }
 
         public void Update()
         {
             throw new NotImplementedException();
+
         }
     }
 }
